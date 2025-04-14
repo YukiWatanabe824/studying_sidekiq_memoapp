941c47c8a78f9e9277a6dc51c3a210e7a118a332
YukiWatanabe824 <gyggg950@gmail.com>
2025-04-14 22:29:02 +0900

    プロジェクトつくりなおし

--- app/jobs/cleanup_memos_job.rb
+++
@@ -1,8 +0,0 @@
-class CleanupNotesJob
-  include Sidekiq::Job
-
-  def perform
-    Note.where('created_at < ?', 1.month.ago).delete_all
-    Rails.logger.info "Deleted notes older than 1 month"
-  end
-end
--- app/models/Notes.rb
+++
@@ -1,3 +0,0 @@
-class Notes < ApplicationRecord
-  
-end
